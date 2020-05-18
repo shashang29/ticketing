677a0e85a823bf172914f6cc520f8847c15d998a
Shashang <51124608+shashang29@users.noreply.github.com>
2020-05-18 17:02:02 +0545

    added abstract error class to enforce uniformitiy

--- auth/src/errors/database-connection-error.ts
+++ auth/src/errors/database-connection-error.ts
@@ -1,9 +1,17 @@
-export class DatabaseConnectionError extends Error {
+import { CustomError } from './custom-error';
+
+export class DatabaseConnectionError extends CustomError {
+    statusCode = 500;
     reason = 'Falied to connect to database'
     constructor() {
-        super();
+        super('Falied to connect to database');
 
         Object.setPrototypeOf(this, DatabaseConnectionError.prototype);
+    }
+    serializeErrors() {
+        return [
+            { message: this.reason }
+        ];
     }
 }
 
--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -1,21 +1,15 @@
 import { Request, Response, NextFunction } from 'express';
 
-import { DatabaseConnectionError } from '../errors/database-connection-error';
-import { RequestValidationError } from '../errors/request-validation-error';
+import { CustomError } from '../errors/custom-error'
 
 export const errorHandler = (
     err: Error,
     req: Request,
     res: Response,
     next: NextFunction) => {
-    if (err instanceof RequestValidationError) {
-        const formattedErrors = err.errors.map(error => {
-            return { message: error.msg, field: error.param }
-        });
-        return res.status(400).send({ errors: formattedErrors })
-    }
-    if (err instanceof DatabaseConnectionError) {
-        return res.status(500).send({ errors: [{ message: err.reason }] })
+
+    if (err instanceof CustomError) {
+        return res.status(err.statusCode).send({ errors: err.serializeErrors() })
     }
 
     res.status(400).send({
